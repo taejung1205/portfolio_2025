679e9db84a14de5285fee870f65c368409ec6d5a
Taejung Kim <immcoc1@gmail.com>
2025-03-10 15:26:36 +0900

    chore: rename Section to SectionBase

---
+++ components/SectionBase.tsx
@@ -0,0 +1,30 @@
+type Props = {
+  children: React.ReactNode;
+  theme: SectionTheme;
+};
+
+export enum SectionTheme {
+  Light = "light",
+  Dark = "dark",
+}
+
+export function SectionBase({ children, theme }: Props) {
+  return (
+    <div
+      className="section"
+      style={{
+        backgroundColor: theme === SectionTheme.Light ? "#ffffff" : "#000F32",
+        color: theme === SectionTheme.Light ? "#000F32" : "#ffffff",
+        position: "relative",
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
