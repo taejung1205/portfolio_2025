3d984b9ff6d86e643497d3d988ceead51d345d68
Taejung Kim <immcoc1@gmail.com>
2025-03-10 11:46:58 +0900

    feat: start text animation after font loaded

--- components/Sections/IntroSection.tsx
+++ components/Sections/IntroSection.tsx
@@ -1,29 +1,38 @@
 import { Section, SectionTheme } from "@components/Section";
 import { CustomText, FontFamily, MotionCustomText } from "@components/Text";
+import { useEffect, useState } from "react";
 
 export default function IntroSection() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    document.fonts.ready.then(() => setFontLoaded(true));
+  }, []);
+
   return (
-    <Section theme={SectionTheme.Dark}>
-      <MotionCustomText
-        fontFamily={FontFamily.Warhaven}
-        fontSize={80}
-        fontWeight={700}
-        initial={{ opacity: 0, y: 40 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, ease: "easeOut" }}
-      >
-        김태정
-      </MotionCustomText>
-      <MotionCustomText
-        fontFamily={FontFamily.Warhaven}
-        fontSize={36}
-        fontWeight={400}
-        initial={{ opacity: 0, y: 40 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, ease: "easeOut", delay: 1 }}
-      >
-        Full-Stack Developer
-      </MotionCustomText>
-    </Section>
+    fontLoaded && (
+      <Section theme={SectionTheme.Dark}>
+        <MotionCustomText
+          fontFamily={FontFamily.Warhaven}
+          fontSize={80}
+          fontWeight={700}
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+        >
+          김태정
+        </MotionCustomText>
+        <MotionCustomText
+          fontFamily={FontFamily.Warhaven}
+          fontSize={36}
+          fontWeight={400}
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: "easeOut", delay: 1 }}
+        >
+          Full-Stack Developer
+        </MotionCustomText>
+      </Section>
+    )
   );
 }
